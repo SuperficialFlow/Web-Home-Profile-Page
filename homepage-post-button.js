23c381755b0aaf919912af18b2f053f02177a96d
JannersLSR <johnrobert.formal@gmail.com>
2024-02-10 23:32:12 +0800

    parity fixes and adding blur

--- homepage-post-button.js
+++ homepage-post-button.js
@@ -1,9 +1,12 @@
 function openPopup() {
     document.getElementById('popupContainer').style.display = 'block';
+    document.getElementById('blurPage').style.filter = 'blur(10px)';
   }
 
   function closePopup() {
     document.getElementById('popupContainer').style.display = 'none';
+    document.getElementById('popupInput').value = '';
+    document.getElementById('blurPage').style.filter = 'none';
   }
 
   function submitPost() {
