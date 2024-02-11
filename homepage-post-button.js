978599ec64edfcca4a635738560b20ce8ffab4ef
JannersLSR <johnrobert.formal@gmail.com>
2024-02-11 12:35:25 +0800

    blur fixed

--- homepage-post-button.js
+++ homepage-post-button.js
@@ -1,11 +1,13 @@
 function openPopup() {
     document.getElementById('popupContainer').style.display = 'block';
-    document.getElementById('blurPage').style.filter = 'blur(10px)';
+    document.getElementById('overlayPage').style.display = 'block';
+    document.getElementById('blurPage').style.filter = 'blur(5px)';
   }
 
   function closePopup() {
     document.getElementById('popupContainer').style.display = 'none';
     document.getElementById('popupInput').value = '';
+    document.getElementById('overlayPage').style.display = 'none';
     document.getElementById('blurPage').style.filter = 'none';
   }
 
